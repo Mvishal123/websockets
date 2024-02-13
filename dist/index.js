fa64c06e66685b2d44c14ef5cdc8e7f28f74be35
Vishal-m <vishalmahesh.study@gmail.com>
2024-02-13 20:16:48 +0530

    webhsockets

---
+++ dist/index.js
@@ -0,0 +1,34 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const http_1 = __importDefault(require("http"));
+const ws_1 = require("ws");
+// Create HTTP server
+const server = http_1.default.createServer();
+// Create WebSocket server
+const wss = new ws_1.WebSocketServer({ server });
+let client = [];
+wss.on("connection", (ws) => {
+    console.log("WebSocket connected...");
+    client.push(ws);
+    ws.on("error", (error) => {
+        console.log("WebSocket error:", error);
+    });
+    ws.on("message", (message) => {
+        // console.log("WebSocket message:", message);
+        client.forEach((c) => {
+            // if (c !== ws) {
+            c.send("Hey " + message.toString());
+            // }
+        });
+    });
+    ws.on("close", () => {
+        console.log("WebSocket closed...");
+    });
+});
+// Start listening on port 8080
+server.listen(8080, () => {
+    console.log("HTTP server listening on port 8080");
+});
---
+++ server/index.ts
@@ -0,0 +1,37 @@
+import http from "http";
+import { WebSocketServer } from "ws";
+
+// Create HTTP server
+const server = http.createServer();
+
+// Create WebSocket server
+const wss = new WebSocketServer({ server });
+
+let client: any = [];
+
+wss.on("connection", (ws) => {
+  console.log("WebSocket connected...");
+  client.push(ws);
+
+  ws.on("error", (error) => {
+    console.log("WebSocket error:", error);
+  });
+
+  ws.on("message", (message) => {
+    // console.log("WebSocket message:", message);
+    client.forEach((c: any) => {
+      // if (c !== ws) {
+        c.send("Hey " + message.toString());
+      // }
+    });
+  });
+
+  ws.on("close", () => {
+    console.log("WebSocket closed...");
+  });
+});
+
+// Start listening on port 8080
+server.listen(8080, () => {
+  console.log("HTTP server listening on port 8080");
+});
